343b419f241beda3ef4c92cc1688ea866746d799
rakibhossen <rakibhossen1660@gmail.com>
2023-12-10 12:01:06 +0600

    footer update

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -1,36 +1,67 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className='grid grid-cols-4 mt-10 p-5 bg-[#FFFFFF]'>
-            <section>
-                <h1 className='font-hammersmith text-3xl'>Rakib's web</h1>
-                <p>We provide web design services for your business and also create content related to programming</p>
-            </section>
-            <section>
-                <h3 className='font-signika text-xl'>Feature Technology</h3>
-                <p className='font-hammersmith text-zinc-400'>HTML</p>
-                <p className='font-hammersmith text-zinc-400'>CSS</p>
-                <p className='font-hammersmith text-zinc-400'>Javascript</p>
-                <p className='font-hammersmith text-zinc-400'>React Js</p>
-            </section>
-            <section>
-                <h2 className='font-signika text-xl'>Supports Technology</h2>
-                <p className='font-hammersmith text-zinc-400'>Tailwind Css</p>
-                <p className='font-hammersmith text-zinc-400'>Bootstrap</p>
-                <p className='font-hammersmith text-zinc-400'>Daisy Ui</p>
-                <p className='font-hammersmith text-zinc-400'>React Roter Dom</p>
-                <p className='font-hammersmith text-zinc-400'>React Component Library</p>
-            </section>
-            <section>
-                <h2 className='font-signika text-xl'>Contact Us</h2>
-                <p className='font-hammersmith text-zinc-400'>Phone: (+088) 1966 366 745</p>
-                <p className='font-hammersmith text-zinc-400'>Email: info@rakib.com</p>
-                <p className='font-hammersmith text-zinc-400'>Whatsapp: @rakib_support</p>
-            </section>
-            <p className='pt-16 pb-10 text-zinc-400'>© 2023 Rakib Co. All rights reserved.</p>
-        </footer>   
-    );
+  return (
+    <div className="px-[100px]">
+      <footer className="flex gap-[60px] mt-[100px] bg-[#FFFFFF]">
+        <section>
+          <h1 className="font-hammersmith text-3xl">Rakib's web</h1>
+          <p className="font-signika text-[16px] mt-[30px]">
+            We provide web design services for your business <br /> and also
+            create content related to programming
+          </p>
+          <h3 className="mt-[30px] font-signika text-[18px] ">
+            Join our news letter
+          </h3>
+          <div className="flex mt-[16px] h-[80px]">
+            <input
+              type="Email"
+              placeholder="Email address"
+              className="font-signika text-[16px] px-[8px] py-[4px] border-[2px] border-[#CFD8DC] rounded-[4px]"
+            />
+            <button className="ml-[4px] font-signika text-[18px] bg-[#1E88E5] text-white rounded-[6px] px-[36px] py-[10px] mt-[24px]">
+              join
+            </button>
+          </div>
+        </section>
+        <section>
+          <h3 className="font-signika text-[24px]">Feature Technology</h3>
+          <p className="mt-[30px] font-signika text-[18px] text-[#979A9A]">
+            HTML
+          </p>
+          <p className="font-signika text-[18px] text-[#979A9A]">CSS</p>
+          <p className="font-signika text-[18px] text-[#979A9A]">Javascript</p>
+          <p className="font-signika text-[18px] text-[#979A9A]">React Js</p>
+        </section>
+        <section>
+          <h2 className="font-signika text-[24px]">Information</h2>
+          <p className="mt-[30px] font-signika text-[18px] text-[#979A9A]">
+            FAQ
+          </p>
+          <p className="font-signika text-[18px] text-[#979A9A]">Site Map</p>
+          <p className="font-signika text-[18px] text-[#979A9A]">
+            Cookies Policy
+          </p>
+          <p className="font-signika text-[18px] text-[#979A9A]">Contact Us</p>
+        </section>
+        <section>
+          <h2 className="font-signika text-[24px]">Contact Us</h2>
+          <p className="mt-[30px] font-signika text-zinc-400">
+            Phone: (+088) 1966 366 745
+          </p>
+          <p className="font-signika text-[18px] text-[#979A9A]">
+            Email: info@rakib.com
+          </p>
+          <p className="font-signika text-[18px] text-[#979A9A]">
+            Whatsapp: @rakib_support
+          </p>
+        </section>
+      </footer>
+      <p className="pt-[150px] pb-[30px] font-signika">
+        © 2023 Rakib Co. All rights reserved.
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../assets/rakib_web_logo.png';
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between p-4'>
-            <h1 className='font-hammersmith'>Rakib's Web</h1>
-            <ul className='flex gap-5 font-signika focus:border-2 border-green-500'>
-                <Link to='/'>Home</Link>
+        <div className='flex justify-between items-center p-4 h-[110px]'>
+            {/* <h1 className='font-hammersmith'>Rakib's Web</h1> */}
+            <Link to='/' className='h-32 w-32 flex items-center'><img src={logo} alt="" /></Link>
+            <ul className='flex gap-5 font-signika'>
+                <NavLink to='/'>Home</NavLink>
                 <Link to='/about'>About</Link>
                 <Link to='/service'>Services</Link>
                 <li>Resume</li>
